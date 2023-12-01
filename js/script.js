a547ba0312a86f6f608fb8002f4bd0d941008124
Fadil Fahrudin <fadilfahrudin32@gmail.com>
2023-12-01 16:58:38 +0700

    update:set skeleton & cek core web vital home page

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import { homePage } from "./home-page.js";
 import { detailLiveVideo } from "./detail-live-video.js";
 import { closeNav, opanNav } from "./navigation.js"
 
+
 // Run all modules
 function runAllModules() {
+
     // Global functions
     closeNav();
     opanNav();
@@ -19,4 +21,7 @@ function runAllModules() {
 // Check Dokument is ready
 document.addEventListener("DOMContentLoaded", function () {
     runAllModules();
+    $(".loading").each(function (i, item) {
+        item.classList.remove("loading");
+    })
 })
