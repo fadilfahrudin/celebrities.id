3997ff849959dce213f4a0be7f07148593dbaca8
Fadil Fahrudin <fadilfahrudin32@gmail.com>
2023-12-29 07:02:22 +0700

    fix: see more in detail-audio

--- js/detail-audio-video.js
+++ js/detail-audio-video.js
@@ -1,12 +1,18 @@
 export const detailAudioVideo = () => {
-    $(".see-more").on("click", () => {
+    const initialText = $(".audio-stream-wrapper p").text();
+    let textOverflow = initialText.length > 340;
+    const sliceText = $(".audio-stream-wrapper p").text().slice(0, 340);
+    if (textOverflow) {
+        $(".audio-stream-wrapper p").text(sliceText);
+        $(".audio-stream-wrapper p").append("<span class='see-more'>see more</span>");
+    }
+    $(".audio-stream-wrapper p").on("click", '.see-more', () => {
         $(".audio-stream-wrapper p").toggleClass("show");
-        $(".see-more").toggleClass("show");
+        if ($(".audio-stream-wrapper p").hasClass("show")) {
+            $(".audio-stream-wrapper p").html(initialText + "<span class='see-more'>show less</span>");
 
-        if ($(".see-more").hasClass("show")) {
-            $(".see-more").text("show less");
         } else {
-            $(".see-more").text("see more");
+            $(".audio-stream-wrapper p").html(sliceText + "<span class='see-more'>see more</span>");
         }
     })
 }
