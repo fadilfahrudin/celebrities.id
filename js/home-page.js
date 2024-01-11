2e53759a40e4a7f6e3ab8178655222861f66f7e3
Fadil Fahrudin <fadilfahrudin32@gmail.com>
2024-01-11 15:32:01 +0700

    update: widget controll btn ketika widget lebih dari 1

--- js/home-page.js
+++ js/home-page.js
@@ -1,13 +1,23 @@
 export const homePage = () => {
-    // Swipe
-    $(".ic-left-arrow").click(() => {
-        $(".live-topic-body").animate({
-            scrollLeft: "-=775px"
-        }, "slow");
-    });
-    $(".ic-right-arrow").click(() => {
-        $(".live-topic-body").animate({
-            scrollLeft: "+=775px"
-        }, "slow");
-    });
+    $(".live-topics .live-topic-body").each((widgetIndex, widgetElement) => {
+        $(".ic-left-arrow").each((indexIcon, iconELement) => {
+            if (widgetIndex == indexIcon) {
+                $(iconELement).click(() => {
+                    $(widgetElement).animate({
+                        scrollLeft: "-=775px"
+                    }, "slow");
+                })
+            }
+        })
+
+        $(".ic-right-arrow").each((indexIcon, iconELement) => {
+            if (widgetIndex == indexIcon) {
+                $(iconELement).click(() => {
+                    $(widgetElement).animate({
+                        scrollLeft: "+=775px"
+                    }, "slow");
+                })
+            }
+        })
+    })
 }
